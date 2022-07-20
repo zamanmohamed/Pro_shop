da586e5d27868be570cb59a54a9b25f6c77f850d
zamanmohamed <zamanmohamed97@gmail.com>
2022-07-20 23:59:44 +0530

    4 004 Modeling Our Data

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`DB Error: ${error}`);
+    console.error(`MongoDB Error: ${error}`);
     process.exit(1);
   }
 };
