bc8a9de4bd867ed75802de54ceb664a9f02584fe
zamanmohamed <zamanmohamed97@gmail.com>
2022-07-25 21:23:47 +0530

    002 User Login Screen & Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
             <Route path="product">
               <Route path=":id" element={<ProductScreen />} />
             </Route>
+            <Route path="login" element={<LoginScreen />} />
             <Route path="cart" element={<CartScreen />}>
               <Route path=":id" element={<CartScreen />} />
             </Route>
