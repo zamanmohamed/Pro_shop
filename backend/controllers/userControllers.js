55275957014424c31171f7a754650ed49185289d
zamanmohamed <zamanmohamed97@gmail.com>
2022-07-26 14:55:30 +0530

    8 005 Update Profile Endpoint

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -79,4 +79,36 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser };
+//@desc Update user profile
+//@route GET/api/users/profile
+//@access Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  //මෙහිදී authMiddleware.js හි cerate කරපු "req.user" variable එක use කරයි --> (JWT (4))
+  const user = await User.findById(req.user._id);
+  console.log(req.user);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,19 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from "../controllers/userControllers.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+
+/*මෙම routes 2ක protect කර ඇති බැවින් 
+headers හි authorization තුල හරිම userට අදාල JWT token 
+එක තිබුනොත් පමණයි මෙම routes වලට ඇතුලුවිය හැක්කෙ*/
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
