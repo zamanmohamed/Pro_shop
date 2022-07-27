98558442c922eb0874a030143a494749213839ea
zamanmohamed <zamanmohamed97@gmail.com>
2022-07-27 22:26:48 +0530

    09 002

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -3,7 +3,7 @@ import { Form, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
-// import CheckoutSteps from '../components/CheckoutSteps'
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from "../actions/cartAction";
 
 const ShippingScreen = () => {
@@ -26,7 +26,7 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
-      {/* <CheckoutSteps step1 step2 /> */}
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
