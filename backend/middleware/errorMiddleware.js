da2a04eb7750665ea64f8d41cb6faa119f9481bc
zamanmohamed <zamanmohamed97@gmail.com>
2022-12-27 21:03:21 +0530

    add

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -3,11 +3,12 @@
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
+  //pass error to next middleware
   next(error);
 };
 
 const errorHandler = (err, req, res, next) => {
-  //statusCode එක 200 තිබුනා නම් 500 ට මාරු කරනවා
+  //statusCode එක 200 තිබුනා  නම් 500 ට මාරු කරනවා
   //statusCode එක 200 නොතිබුනා නම් එහි අගය වෙනස් කරන්නෙ නැත
 
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
